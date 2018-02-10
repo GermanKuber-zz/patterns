ea9a79a214aea80d7c0d439803de4d55670e454c
Germán Küber <german.kuber@outlook.com>
2018-02-10 18:03:31 -0300

    Added test to Provider

--- Entities.Tests/ProviderTest.cs
+++ Entities.Tests/ProviderTest.cs
@@ -1,14 +1,20 @@
-using System;
 using Xunit;
 
 namespace Entities.Tests
 {
     public class ProviderTest
     {
-        [Fact]
-        public void Test1()
-        {
+        private Provider _sut;
 
+        public ProviderTest()
+        {
+            _sut = new Provider { Id = 1, Name = "Test" };
+        }
+        [Fact]
+        public void CompareTo_ShouldBe_Equals_Compare_Id()
+        {
+            var providerToCompare = new Provider { Id = 1 };
+            Assert.Equal(0, _sut.CompareTo(providerToCompare));
         }
     }
 }
