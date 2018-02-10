375f6a2025da18d1e534909db12f2a0c650b6d96
Germán Küber <german.kuber@outlook.com>
2018-02-10 12:12:55 -0300

    Added SetStrategy in the Decorator

--- Entities/Interfaces/Decorator/Decorator.cs
+++ Entities/Interfaces/Decorator/Decorator.cs
@@ -1,12 +1,21 @@
-﻿namespace Entities.Interfaces
+﻿using Implementations.ChainOfResponsibility.Decorator;
+
+namespace Entities.Interfaces
 {
     public abstract class Decorator<TUpdating,  TParameters> : IStrategy<TUpdating, TParameters>, IDecorator<TUpdating, TParameters> where TParameters:IParameters
     {
         protected IStrategy<TUpdating, TParameters> StrategyToDecorate;
+   
         public void SetStrategy(IStrategy<TUpdating, TParameters> strategyToDecorate)
         {
             StrategyToDecorate = strategyToDecorate;
         }
+
+        public void SetStrategy<TEntityToProcess, TParameter>(DecoratorStepAuction<TEntityToProcess, TParameter> chainOfSteps)
+        {
+        }
+
+        
         public virtual void Execute(TUpdating updatingEntity, TParameters parameter)
         {
             if (StrategyToDecorate != null)
